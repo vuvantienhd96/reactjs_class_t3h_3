595fc0002801ef76e92571b42169a89c63c52085
tienvv11 <congtuocxauxa000@gmail.com>
2022-02-27 16:57:20 +0700

    add state and setState to Contact

--- bai-1-create-app/src/components/Contact.js
+++ bai-1-create-app/src/components/Contact.js
@@ -3,20 +3,32 @@ import PropTypes from 'prop-types';
 import './Contact.css';
 
 export default class Contact extends Component {
-  render() {
     
-    const {name, phone, email} = this.props.contact;
-    
-    return (
-        <div className='card card-body mb-3'>
-            <h4>{name}</h4>
-            <ul className='list-group'>
-                <li className='list-group-item'>Email: {email}</li>
-                <li className='list-group-item'>Phone: {phone}</li>
-            </ul>
-        </div>
-    )
-  }
+    constructor(props){
+        super(props);
+        this.state = {
+            onShow: false
+        }
+    }
+
+    onShowClick = () => {
+        this.setState({
+            onShow: !this.state.onShow
+        })
+    }
+
+    render() {
+        const { name, phone, email } = this.props.contact;
+        return (
+            <div className='card card-body mb-3'>
+                <h4>{name} <i className='fas fa-sort-down' onClick={this.onShowClick} /></h4>
+                {this.state.onShow && <ul className='list-group'>
+                    <li className='list-group-item'>Email: {email}</li>
+                    <li className='list-group-item'>Phone: {phone}</li>
+                </ul>}
+            </div>
+        )
+    }
 }
 
 
